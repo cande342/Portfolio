a53c9e1d97c6f286cc1f40af52a4363bb47ffbf4
Candela <gcandela894@gmail.com>
2024-03-11 12:42:31 -0300

    configuración para despliegue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { ProyectsComponent } from './components/proyects/proyects.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 
+
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'proyects', component: ProyectsComponent },
