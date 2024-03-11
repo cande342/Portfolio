0b0d53969985953f8e78fe028be040bc972a2efe
Candela <gcandela894@gmail.com>
2024-03-11 14:01:11 -0300

    bajamos la pagina de guthub

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProyectsComponent } from './components/proyects/proyects.component';
-import { AboutMeComponent } from './components/about-me/about-me.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'proyects', component: ProyectsComponent },
-    { path: 'about-me', component: AboutMeComponent  },
 ];
